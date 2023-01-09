cb0ff0d2be563abc67addcc2ec10e2608e836a9c
cypher20b <nuel.emma20@gmail.com>
2023-01-09 00:51:56 +0000

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { SurveyComponent } from './components/survey/survey.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SurveyComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
